6687919d1b9b72e39d8af81de2cac7ae695daf81
Santiago G. Medina <santiagogmm@hotmail.com>
2018-08-21 09:41:06 -0500

    Codon Composition function added

--- R/count_codons.R
+++ R/count_codons.R
@@ -22,3 +22,44 @@ count_codons <- function(seq) {
     tibble::as_tibble() %>%
     dplyr::rename_(codon = ".")
 }
+
+
+#' Codon Composition of Sequences
+#'
+#' @param orfs tibble, data with cds sequences
+#' @param orf_col character, column name containing cds
+#' @param id_col character, column name with gene/name id
+#' @param cores integer, number of course to use
+#'
+#' @return tible n by x, codon counts for each sequence, n is the number
+#' of sequences and x is the number of codons (x=61) it may contain more thant
+#' 61 in case there is a codon loke ACN
+#' @export
+#' @importFrom magrittr %>%
+#' @examples
+#' orf <- tibble::tibble(cds= c("ACGTTT", "TTTCCC"), id=c("s0","s1"))
+#' codon_composition(orf, orf_col = "cds", id_col = "id")
+codon_composition <- function(orfs, orf_col, id_col, cores=4) {
+
+  # rename the columns for easy manipulation
+  orfs <- dplyr::rename_(orfs, "cds_seq" = orf_col, "id_col" = id_col)
+  n_ids <- length(unique(orfs$id_col))
+  if (n_ids != nrow(orfs)){
+    stop("id_col should contain unique identifiers")
+  }
+  composition_tb <-
+    orfs %>%
+    split(.$id_col) %>%
+    parallel::mclapply(
+      function(x) {
+        count_codons(x$cds_seq) %>%
+          dplyr::mutate(id_col = x$id_col)
+      },
+      mc.cores = cores
+    ) %>%
+    do.call(what = dplyr::bind_rows) %>%
+    tidyr::spread(key = codon, value = n) %>%
+    dplyr::mutate_if(is.numeric, dplyr::funs(replace(., is.na(.), 0)))
+
+  return(composition_tb)
+}
--- tests/testthat/test_count_codons.R
+++ tests/testthat/test_count_codons.R
@@ -16,3 +16,38 @@ test_that('correct number of counts', {
 })
 
 
+context("codon composition")
+# test codon composition function -----------------------------------------
+
+## create today data set
+set.seed(10)
+n <- 100
+## generate fake data set for testing
+Reduce(function(x, y) paste0(x, y), sample(c("A", "C", "G", "T"), n*3, replace = T))
+cds <- replicate(
+  n,
+  expr = Reduce(
+    function(x, y) paste0(x, y),
+    sample(c("A", "C", "G", "T"), rpois(n = 1, lambda = 100)*3, replace = T)
+    )
+  )
+ids <- paste0("id_", rnorm(100))
+data <- tibble::tibble(ids = ids, cds = cds)
+output <- codon_composition(data, id_col = "ids", orf_col = "cds")
+
+test_that('correct output', {
+  expect_equal(nrow(data), nrow(output))
+  expect_false(any(duplicated(output$id_col)))
+  expect_error(codon_composition(dplyr::bind_rows(data, data), , id_col = "ids", orf_col = "cds"))
+  # total sum equal length
+  result <-
+    tidyr::gather(output, key="codon", value="n", -id_col) %>%
+    dplyr::group_by(id_col) %>%
+    dplyr::summarise(total_codons = sum(n)) %>%
+    dplyr::inner_join(data, by=c("id_col" = "ids")) %>%
+    dplyr::mutate(expected_codons = nchar(cds) / 3)
+  expect_true(all(result$total_codons == result$expected_codons))
+
+})
+
+
