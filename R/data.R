1fdd2e8d3b9ab001ecf5ade469750d57296932b0
Santiago G. Medina <santiagogmm@hotmail.com>
2019-09-24 13:39:09 -0500

    added machine learning datasets

--- R/data.R
+++ R/data.R
@@ -10,3 +10,37 @@
 #' }
 #'
 "optimality_code_embo2016"
+
+#' @title Train data
+#' @description Data used to train mRNA stability predictor, this data set
+#' was splitted such that the same gene is not in the training and testing
+#' sets at the same time
+#' @format A data frame with 67775 rows and 7 variables:
+#' \describe{
+#'   \item{\code{gene_id}}{character ensembl gene id}
+#'   \item{\code{specie}}{character the specie}
+#'   \item{\code{cell_type}}{character cell type where mRNA stability comes}
+#'   \item{\code{datatype}}{character technique used to generate mRNA stability profile}
+#'   \item{\code{decay_rate}}{double mRNA decay rate, scaled for each stability profile}
+#'   \item{\code{utrlenlog}}{double log length of 3' UTR}
+#'   \item{\code{coding}}{character ORF star to stop codon}
+#'}
+#' @source \url{https://github.com/santiago1234/MZT-rna-stability/blob/master/results/19-04-30-PredictiveModelDecayAllSpecies/19-04-30-EDA/EDAanalysos.md}
+"train_set"
+
+#' @title Test data
+#' @description Data used to evaluate mRNA stability predictor, this data set
+#' was splitted such that the same gene is not in the training and testing
+#' sets at the same time
+#' @format A data frame with 67775 rows and 7 variables:
+#' \describe{
+#'   \item{\code{gene_id}}{character ensembl gene id}
+#'   \item{\code{specie}}{character the specie}
+#'   \item{\code{cell_type}}{character cell type where mRNA stability comes}
+#'   \item{\code{datatype}}{character technique used to generate mRNA stability profile}
+#'   \item{\code{decay_rate}}{double mRNA decay rate, scaled for each stability profile}
+#'   \item{\code{utrlenlog}}{double log length of 3' UTR}
+#'   \item{\code{coding}}{character ORF star to stop codon}
+#'}
+#' @source \url{https://github.com/santiago1234/MZT-rna-stability/blob/master/results/19-04-30-PredictiveModelDecayAllSpecies/19-04-30-EDA/EDAanalysos.md}
+"test_set"
