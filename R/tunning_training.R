35d1c330aa87fe754a8587d0dcab49f60d1b02aa
Santiago G. Medina <santiagogmm@hotmail.com>
2019-09-30 12:55:44 -0500

    Save each model as soon as its trained I modified the trian function to save the model as soon as it saves, this will help me in case an error happen I do not need to retrain all the models

--- R/tunning_training.R
+++ R/tunning_training.R
@@ -18,6 +18,7 @@ cross_fold_validation <- function(k = 10) {
 }
 
 
+
 #' Train Machine Learning models to predict mRNA stability
 #'
 #' Evaluates a set of machine learning models, also perfoms hyperparameter tunning.
@@ -25,11 +26,28 @@ cross_fold_validation <- function(k = 10) {
 #' This function uses the \code{datos_preprocessed} training data for tunning,
 #' @param ncores integer number of cores for parallel processing. set to the
 #' numbers of cores in your machine
+#' @param output_dir_to_save_models character: output directory to save trained models
 #'
-#' @return list of trained models
+#' @return None, all the model if not trained will be saved to \code{output_dir_to_save_models}
 #' @export
 #' @note This function is computational expensive and it requires parallel processing
-tune_models <- function(ncores = 32) {
+tune_models <- function(ncores = 32, output_dir_to_save_models = "results-trained-models") {
+  # helper functions to check if model exists and save them
+
+  make_model_id_name <- function(mdl_name) {
+    file.path(output_dir_to_save_models, paste0(mdl_name, ".rds"))
+  }
+
+  has_model_been_trained <- function(mdl_name) {
+    # returns true if the model exits
+    file.exists(make_model_id_name(mdl_name))
+  }
+
+  save_model <- function(mdl, mld_name) {
+    message(paste0("saving model to ", make_model_id_name(mdl_name), " ..."))
+    readr::write_rds(mld, make_model_id_name(mdl_name))
+  }
+
   # parallel processing for grid search
   cl <- parallel::makePSOCKcluster(ncores)
   doParallel::registerDoParallel(cl)
@@ -38,187 +56,252 @@ tune_models <- function(ncores = 32) {
   control_object <- cross_fold_validation()
 
   # LINEAR MODELS
-  message("linear regression ...")
-  set.seed(669)
-  linear_reg <- caret::train(
-    x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
-    method = "lm",
-    trControl = control_object,
-    metric = "Rsquared"
-  )
-
-  message("pls regression ...")
-  set.seed(669)
-  pls_model <- caret::train(
-    x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
-    method = "pls",
-    tuneLength = 15,
-    trControl = control_object,
-    metric = "Rsquared"
-  )
-
-  message("elastic net regression ...")
-  set.seed(669)
-  enet_grid <- expand.grid(
-    lambda = c(0, .001, .01, .1),
-    fraction = seq(0.05, 1, length.out = 20)
-  )
-
-  enet_model <- caret::train(
-    x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
-    method = "enet",
-    tuneGrid = enet_grid,
-    trControl = control_object,
-    metric = "Rsquared"
-  )
-
-  # NON LINEAR MODELS
-
-  message("earth regression ...")
-  set.seed(669)
-  earth_model <- caret::train(
-    x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
-    method = "earth",
-    tuneGrid = expand.grid(degree = 1, nprune = 2:25),
-    trControl = control_object,
-    metric = "Rsquared"
-  )
+  mdl_name <- "linear"
+  if (!has_model_been_trained(mdl_name)) {
+    message("linear regression ...")
+    set.seed(669)
+    linear_reg <- caret::train(
+      x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
+      method = "lm",
+      trControl = control_object,
+      metric = "Rsquared"
+    )
+    save_model(linear_reg, mdl_name)
+  }
 
 
-  message("svm regression ...")
-  set.seed(669)
-  svmr_model <- caret::train(
-    x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
-    method = "svmLinear",
-    tuneLength = 10,
-    trControl = control_object,
-    metric = "Rsquared"
-  )
+  mdl_name <- "pls"
+  if (!has_model_been_trained(mdl_name)) {
+    message("pls regression ...")
+    set.seed(669)
+    pls_model <- caret::train(
+      x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
+      method = "pls",
+      tuneLength = 15,
+      trControl = control_object,
+      metric = "Rsquared"
+    )
+    save_model(pls_model, mdl_name)
+  }
 
-  message("neural network regression ...")
-  set.seed(669)
-  nnet_grid <- expand.grid(
-    decay = c(.001, .01, .1),
-    size = seq(1, 27, by = 2),
-    bag = FALSE
-  )
-  nnet_model <- caret::train(
-    x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
-    method = "avNNet",
-    tuneGrid = nnet_grid,
-    trControl = control_object,
-    metric = "Rsquared"
-  )
+  mld_name <- "enet"
+  if (!has_model_been_trained(mdl_name)) {
+    message("elastic net regression ...")
+    set.seed(669)
+    enet_grid <- expand.grid(
+      lambda = c(0, .001, .01, .1),
+      fraction = seq(0.05, 1, length.out = 20)
+    )
 
-  message("k-nearest neighbors regression ...")
-  set.seed(669)
-  knn_model <- caret::train(
-    x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
-    method = "knn",
-    tuneGrid = base::data.frame(k = 1:20),
-    trControl = control_object,
-    metric = "Rsquared"
-  )
+    enet_model <- caret::train(
+      x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
+      method = "enet",
+      tuneGrid = enet_grid,
+      trControl = control_object,
+      metric = "Rsquared"
+    )
+    save_model(enet_model, mdl_name)
+  }
 
 
-  # REGRESSION AND MODEL TRESS
+  mdl_name <- "earth"
+  if (!has_model_been_trained(mdl_name)) {
+    message("earth regression ...")
+    set.seed(669)
+    earth_model <- caret::train(
+      x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
+      method = "earth",
+      tuneGrid = expand.grid(degree = 1, nprune = 2:25),
+      trControl = control_object,
+      metric = "Rsquared"
+    )
+    save_model(earth_model, mdl_name)
+  }
 
-  message("rpart  regression ...")
-  set.seed(669)
-  rpart_model <- caret::train(
-    x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
-    method = "rpart",
-    tuneLength = 30,
-    trControl = control_object,
-    metric = "Rsquared"
-  )
 
-  message("ctree  regression ...")
-  set.seed(669)
-  ctree_model <- caret::train(
-    x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
-    method = "ctree",
-    tuneLength = 10,
-    trControl = control_object,
-    metric = "Rsquared"
-  )
+  mdl_name <- "svm"
+  if (!has_model_been_trained(mdl_name)) {
+    message("svm regression ...")
+    set.seed(669)
+    svmr_model <- caret::train(
+      x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
+      method = "svmLinear",
+      tuneLength = 10,
+      trControl = control_object,
+      metric = "Rsquared"
+    )
+    save_model(svmr_model, mdl_name)
+  }
 
-  message("mtree  regression ...")
-  set.seed(669)
-  mt_model <- caret::train(
-    x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
-    method = "M5",
-    trControl = control_object,
-    metric = "Rsquared"
-  )
 
-  # ENSEMBLE METHODS
+  mdl_name <- "nnet"
+  if (!has_model_been_trained(mdl_name)) {
+    message("neural network regression ...")
+    set.seed(669)
+    nnet_grid <- expand.grid(
+      decay = c(.001, .01, .1),
+      size = seq(1, 27, by = 2),
+      bag = FALSE
+    )
+    nnet_model <- caret::train(
+      x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
+      method = "avNNet",
+      tuneGrid = nnet_grid,
+      trControl = control_object,
+      metric = "Rsquared"
+    )
+    save_model(nnet_model, mdl_name)
+  }
 
-  message("random forest regression ...")
-  set.seed(669)
-  rf_model <- caret::train(
-    x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
-    method = "rf",
-    tuneLength = 10,
-    ntrees = 1000,
-    importance = TRUE,
-    trControl = control_object,
-    metric = "Rsquared"
-  )
+  mdl_name <- "knn"
+  if (!has_model_been_trained(mdl_name)) {
+    message("k-nearest neighbors regression ...")
+    set.seed(669)
+    knn_model <- caret::train(
+      x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
+      method = "knn",
+      tuneGrid = base::data.frame(k = 1:20),
+      trControl = control_object,
+      metric = "Rsquared"
+    )
+    save_model(knn_model, mdl_name)
+  }
 
-  message("gradient boosting regression ...")
-  set.seed(669)
-  gbm_grid <- expand.grid(
-    interaction.depth = seq(1, 7, by = 2),
-    n.trees = seq(100, 1000, by = 50),
-    shrinkage = c(0.01, 0.1, 0.5),
-    n.minobsinnode = 10
-  )
-  gbm_model <- caret::train(
-    x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
-    method = "gbm",
-    tuneGrid = gbm_grid,
-    verbose = FALSE,
-    trControl = control_object,
-    metric = "Rsquared"
-  )
 
-  message("cubist regression ...")
-  set.seed(669)
-  cubist_grid <- expand.grid(
-    commmitees = c(1, 5, 10, 50, 75, 100),
-    neighbors = c(0, 1, 3, 5, 7, 9)
-  )
-  cubist_model <- caret::train(
-    x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
-    method = "cubist",
-    tuneGrid = cubist_grid,
-    trControl = control_object,
-    metric = "Rsquared"
-  )
+  mdl_name <- "rpart"
+  if (!has_model_been_trained(mdl_name)) {
+    message("rpart  regression ...")
+    set.seed(669)
+    rpart_model <- caret::train(
+      x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
+      method = "rpart",
+      tuneLength = 30,
+      trControl = control_object,
+      metric = "Rsquared"
+    )
+    save_model(rpart_model, mdl_name)
+  }
 
-  # collect results
-  all_models <- list(
-    "linear regression" = linear_reg,
-    "partial least squares" = pls_model,
-    "elastic net" = enet_model,
-    "adaptative regression spline" = earth_model,
-    "support vector machine" = svmr_model,
-    "neural network" = nnet_model,
-    "k-nearest neighbors" = knn_model,
-    "single tree" = rpart_model,
-    "model tree" = mt_model,
-    "c tree" = ctree_model,
-    "random forest" = rf_model,
-    "boosted trees" = gbm_model,
-    "cubist" = cubist_model
-  )
 
-  parallel::stopCluster(cl) # stop cluster
-  all_models
+  mdl_name <- "ctree"
+  if (!has_model_been_trained(mdl_name)) {
+    message("ctree  regression ...")
+    set.seed(669)
+    ctree_model <- caret::train(
+      x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
+      method = "ctree",
+      tuneLength = 10,
+      trControl = control_object,
+      metric = "Rsquared"
+    )
+    save_model(ctree_model, mdl_name)
+  }
+
+  mdl_name <- "mtree"
+  if (!has_model_been_trained(mdl_name)) {
+    message("mtree  regression ...")
+    set.seed(669)
+    mt_model <- caret::train(
+      x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
+      method = "M5",
+      trControl = control_object,
+      metric = "Rsquared"
+    )
+    save_model(mt_model, mdl_name)
+  }
+
+
+  mdl_name <- "random_forest"
+  if (!has_model_been_trained(mdl_name)) {
+    message("random forest regression ...")
+    set.seed(669)
+    rf_model <- caret::train(
+      x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
+      method = "rf",
+      tuneLength = 10,
+      ntrees = 1000,
+      importance = TRUE,
+      trControl = control_object,
+      metric = "Rsquared"
+    )
+    save_model(rf_model, mdl_name)
+  }
+
+  mdl_name <- "gbm"
+  if (!has_model_been_trained(mdl_name)) {
+    message("gradient boosting regression ...")
+    set.seed(669)
+    gbm_grid <- expand.grid(
+      interaction.depth = seq(1, 7, by = 2),
+      n.trees = seq(100, 1000, by = 50),
+      shrinkage = c(0.01, 0.1, 0.5),
+      n.minobsinnode = 10
+    )
+    gbm_model <- caret::train(
+      x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
+      method = "gbm",
+      tuneGrid = gbm_grid,
+      verbose = FALSE,
+      trControl = control_object,
+      metric = "Rsquared"
+    )
+    save_model(gbm_model, mdl_name)
+  }
+
+  mdl_name <- "cubist"
+  if (!has_model_been_trained(mdl_name)) {
+    message("cubist regression ...")
+    set.seed(669)
+    cubist_grid <- expand.grid(
+      commmitees = c(1, 5, 10, 50, 75, 100),
+      neighbors = c(0, 1, 3, 5, 7, 9)
+    )
+    cubist_model <- caret::train(
+      x = datos_preprocessed$X_train, y = datos_preprocessed$y_train,
+      method = "cubist",
+      tuneGrid = cubist_grid,
+      trControl = control_object,
+      metric = "Rsquared"
+    )
+    save_model(cubist_model)
+  }
+
+  # stop cluster
+  parallel::stopCluster(cl)
 }
 
 
-train_final_model <- function(X_train = datos_preprocessed$X_train, y_train = datos_preprocessed$y_train) {
-  NULL
+#' Train Final Model
+#'
+#' This function trains the final model with
+#' the final hyperparameters found after model
+#' tunning
+#'
+#' @param X_train train predictors
+#' @param y_train test response
+#'
+#' @return trained gradient boosted model with gaussian loss function.
+#' @export
+#' @note when using the output model for prediction, the
+#' data should be a data.frame object and not a tibble, if not
+#' the function will throw an error
+train_final_model <- function(
+                              X_train = datos_preprocessed$X_train,
+                              y_train = datos_preprocessed$y_train) {
+
+  # the X_train has be be a data.fram in order to preict
+  # and train with gbm
+  X_train <- base::as.data.frame(X_train)
+
+  gbm::gbm.fit(
+    # this model does no accept the tibble only the data frame
+    x = X_train,
+    y = y_train,
+    distribution = "gaussian",
+    n.trees = 750,
+    interaction.depth = 5,
+    shrinkage = 0.1,
+    n.minobsinnode = 10,
+    verbose = TRUE
+  )
 }
